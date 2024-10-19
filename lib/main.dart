71ca1d3ebcb59bab2f9afc274b293a7955240d2f
ISHA <itank861@rku.ac.in>
2024-10-19 14:43:46 +0530

    Whole Project

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:book_grocer/common/color_extenstion.dart';
+import 'package:book_grocer/view/main_tab/main_tab_view.dart';
+import 'package:book_grocer/view/onboarding/onboarding_view.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  // Ensure Firebase is initialized before running the app
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: TColor.primary,  // Assuming TColor is defined in color_extension.dart
+        fontFamily: 'SF Pro Text',
+      ),
+      home: const MainTabView(),
+    );
+  }
+}
